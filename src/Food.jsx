20e1394876060302ef1ca2c1aad78a536ab95386
road12659 <raymondadudanso@gmail.com>
2025-06-23 08:42:24 +0000

    Files uploads

---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+
+function Food(){
+
+    const name1 = "Raymond";
+    const name2 = "Owuraba";
+    const name3 = "Adu-Danso";
+
+    return(
+
+            <p>
+                <h2>My name is {name1} {name2} {name3}</h2>             
+            </p>
+    );
+}
+
+export default Food
---
+++ src/Hobbies.jsx
@@ -0,0 +1,22 @@
+
+function Footer(){
+     return(
+        <div>
+            <p>
+                I am a very passionate reader and love making people happy. I really love being around 
+                gadgets be it phones or laptops and that is what got me into learning Web Development.
+            </p>
+            <p>The following are my hobbies and passion</p>
+            <ul>
+                <li>Listening to music</li>
+                <li>Reading</li>
+                <li>Researching</li>
+                <li>Dancing</li>
+                <li>Watching movies</li>
+                <li>Day Dreaming</li>
+            </ul>
+        </div>
+     );
+}
+
+export default Footer
